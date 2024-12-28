3b540f1075ca722e0f59100c9abb44f6b3c2d6e1
Jayant Yadav <darksoul00900@gmail.com>
2024-12-29 01:03:36 +0530

    final changes

--- random_password_generator/password_generator.js
+++ random_password_generator/password_generator.js
@@ -21,55 +21,70 @@ button.onclick = function() {
     let valchang = input.value;
     let valchang1 = Number(valchang);
 
-    if(lowercase.checked === true) {
-        altpassword += lowercaseChars;
+    if(lowercase.checked === true && uppercase.checked === true && numbers.checked === true && symbols.checked === true) {
+        altpassword += lowercaseChars + uppercaseChars + numberChars + symbolChars;
     }
-    else if(uppercase.checked === true) {
-        altpassword += uppercaseChars;
-    }
-    else if(numbers.checked === true) {
-        altpassword += numberChars;
-    }
-    else if(symbols.checked === true) {
-        altpassword += symbolChars;
-    }
-
-
-    else if(lowercase.checked === true && uppercase.checked === true) {
-        altpassword += lowercaseChars + uppercaseChars;
-    }
-    else if(lowercase.checked === true && numbers.checked === true) {
-        altpassword += lowercaseChars + numberChars;
-    }
-    else if(lowercase.checked === true && symbols.checked === true) {
-        altpassword += lowercaseChars + symbolChars;
-    }
-    else if(uppercase.checked === true && numbers.checked === true) {
-        altpassword += uppercaseChars + numberChars;
-    }
-    else if(uppercase.checked === true && symbols.checked === true) {
-        altpassword += uppercaseChars + symbolChars;
-    }
-    else if(numbers.checked === true && symbols.checked === true) {
-        altpassword += numberChars + symbolChars;
-    }
-
-    else if(lowercase.checked === true && uppercase.checked === true && numbers.checked === true) {
-        altpassword += lowercaseChars + uppercaseChars + numberChars;
-    }
-    else if(lowercase.checked === true && uppercase.checked === true && symbols.checked === true) {
-        altpassword += lowercaseChars + uppercaseChars + symbolChars;
-    }
-    else if(lowercase.checked === true && numbers.checked === true && symbols.checked === true) {
-        altpassword += lowercaseChars + numberChars + symbolChars;
-    }
+    
     else if(uppercase.checked === true && numbers.checked === true && symbols.checked === true) {
         altpassword += uppercaseChars + numberChars + symbolChars;
     }
-
-    else if(lowercase.checked === true && uppercase.checked === true && numbers.checked === true && symbols.checked === true) {
-        altpassword += lowercaseChars + uppercaseChars + numberChars + symbolChars;
+    
+    else if(lowercase.checked === true && numbers.checked === true && symbols.checked === true) {
+        altpassword += lowercaseChars + numberChars + symbolChars;
     }
+    
+    else if(lowercase.checked === true && uppercase.checked === true && symbols.checked === true) {
+        altpassword += lowercaseChars + uppercaseChars + symbolChars;
+    }
+    
+    else if(lowercase.checked === true && uppercase.checked === true && numbers.checked === true) {
+        altpassword += lowercaseChars + uppercaseChars + numberChars;
+    }
+    
+    else if(numbers.checked === true && symbols.checked === true) {
+        altpassword += numberChars + symbolChars;
+    }
+    
+    else if(uppercase.checked === true && symbols.checked === true) {
+        altpassword += uppercaseChars + symbolChars;
+    }
+    
+    else if(uppercase.checked === true && numbers.checked === true) {
+        altpassword += uppercaseChars + numberChars;
+    }
+    
+    else if(lowercase.checked === true && symbols.checked === true) {
+        altpassword += lowercaseChars + symbolChars;
+    }
+    
+    else if(lowercase.checked === true && numbers.checked === true) {
+        altpassword += lowercaseChars + numberChars;
+    }
+    
+    else if(lowercase.checked === true && uppercase.checked === true) {
+        altpassword += lowercaseChars + uppercaseChars;
+    }
+    
+    else if(symbols.checked === true) {
+        altpassword += symbolChars;
+    }
+    
+    else if(numbers.checked === true) {
+        altpassword += numberChars;
+    }
+    
+    else if(uppercase.checked === true) {
+        altpassword += uppercaseChars;
+    }
+    
+    else if(lowercase.checked === true) {
+        altpassword += lowercaseChars;
+    }
+    else (lowercase.checked === false && uppercase.checked === false && numbers.checked === false && symbols.checked === false)
+    {
+        window.alert("Please select at least one character type");
+    }
+    
 
     for(let i = 0; i < valchang1; i++) {
         password += altpassword.charAt(Math.floor(Math.random() * altpassword.length));
